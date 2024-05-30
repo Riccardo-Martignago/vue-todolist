9b69e9739e23352f62f39adc72862331ab2ff009
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-30 15:25:32 +0200

    Aggiunto array con gli oggetti al suo interno e stampato in pagina la proprietá text di ogni oggetto

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,32 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'ciao',
+            todos : [
+                {
+                    text: "Fare la spesa",
+                    done: false
+                },
+                {
+                    text: "Portare a spasso il cane",
+                    done: false
+                },
+                {
+                    text: "Studiare per l'esame",
+                    done: true
+                },
+                {
+                    text: "Pulire la casa",
+                    done: false
+                },
+                {
+                    text: "Chiamare il dottore",
+                    done: true
+                }
+            ],
+            
         }
+    },
+    methods:{
+
     }
 }).mount('#app')
